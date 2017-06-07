7b9a93bcb0abfc43d372cf9aa71908affd1edd90
Ruoyu Fu <lingoerer@gmail.com>
2017-06-07 13:20:53 +0800

    Initial Commit

---
+++ CrossGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrossGuide
+//
+//  Created by Ruoyu Fu on 7/6/2017.
+//  Copyright © 2017 Ruoyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
